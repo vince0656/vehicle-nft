d463df8c2793bb4d759135e4e18cf37faa2f1212
Vincent de Almeida <vincent@decentralisedengineeringltd.co.uk>
2020-11-22 12:03:46 +0000

    consume name and symbol in constructor

--- smart-contracts/contracts/Vehicle.sol
+++ smart-contracts/contracts/Vehicle.sol
@@ -3,7 +3,9 @@
 pragma solidity 0.7.5;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "./ERC998/IERC998ERC721TopDown.sol";
+import "./ERC998/IERC998ERC721TopDownEnumerable.sol";
 
-contract Vehicle is ERC721("Vehicle", "VCL") {
-    constructor() {}
+contract Vehicle is ERC721 {
+    constructor(string memory _manufacturer, string memory _symbol) ERC721(_manufacturer, _symbol) {}
 }
