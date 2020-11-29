a00bf090c5c39ded91c707b349b03c8445080169
Vincent de Almeida <vincent@decentralisedengineeringltd.co.uk>
2020-11-29 10:34:30 +0000

    start of 998 implementation

--- smart-contracts/contracts/Vehicle.sol
+++ smart-contracts/contracts/Vehicle.sol
@@ -6,7 +6,14 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "./ERC998/IERC998ERC721TopDown.sol";
 import "./ERC998/IERC998ERC721TopDownEnumerable.sol";
 
-contract Vehicle is ERC721, IERC998ERC721TopDown {
+contract Vehicle is ERC721, IERC998ERC721TopDown, IERC998ERC721TopDownEnumerable {
+    // return this.rootOwnerOf.selector ^ this.rootOwnerOfChild.selector ^
+    //   this.tokenOwnerOf.selector ^ this.ownerOfChild.selector;
+    bytes32 constant ERC998_MAGIC_VALUE = 0xcd740db5;
+
+    // child address => childId => tokenId
+    mapping(address => mapping(uint256 => uint256)) internal childTokenOwner;
+
     constructor(string memory _manufacturer, string memory _symbol) ERC721(_manufacturer, _symbol) {}
 
 //    function stringToBytes32(string memory source) public pure returns (bytes32 result) {
@@ -25,7 +32,20 @@ contract Vehicle is ERC721, IERC998ERC721TopDown {
     }
 
     function rootOwnerOfChild(address _childContract, uint256 _childTokenId) public override view returns (bytes32 rootOwner) {
-        return 0x0;
+        address rootOwnerAddress;
+
+        // When zero passed in, the query is about who owns the Vehicle token ID specified by [_childTokenId] param
+        if (_childContract == address(0)) {
+            rootOwnerAddress = ownerOf(_childTokenId);
+        }
+
+        (rootOwnerAddress,) = _ownerOfChild(_childContract, _childTokenId);
+
+        // todo, check that the child is whitelisted
+        // todo, check that the child is linked to a parent
+
+        // Ownership of a child is implicit from the ownership of a vehicle (NFT)
+        return ERC998_MAGIC_VALUE << 224 | bytes32(rootOwnerAddress);
     }
 
     function ownerOfChild(address _childContract, uint256 _childTokenId) external override view returns (bytes32 parentTokenOwner, uint256 parentTokenId) {
@@ -56,5 +76,28 @@ contract Vehicle is ERC721, IERC998ERC721TopDown {
     // The _childContract must approve this contract. Then getChild can be called.
     function getChild(address _from, uint256 _tokenId, address _childContract, uint256 _childTokenId) external override {
 
+    }
+
+    // enumerable interface
+    function totalChildContracts(uint256 _tokenId) external view returns (uint256) {
+        return 0;
+    }
+
+    function childContractByIndex(uint256 _tokenId, uint256 _index) external view returns (address childContract) {
+        return address(0);
+    }
+
+    function totalChildTokens(uint256 _tokenId, address _childContract) external view returns (uint256) {
+        return 0;
+    }
+
+    function childTokenByIndex(uint256 _tokenId, address _childContract, uint256 _index) external view returns (uint256 childTokenId) {
+        return 0;
+    }
+
+    function _ownerOfChild(address _childContract, uint256 _childTokenId) internal view returns (address parentTokenOwner, uint256 parentTokenId) {
+        parentTokenId = childTokenOwner[_childContract][_childTokenId];
+        require(parentTokenId > 0 || childTokenIndex[parentTokenId][_childContract][_childTokenId] > 0);
+        return (tokenIdToTokenOwner[parentTokenId], parentTokenId);
     }
 }
