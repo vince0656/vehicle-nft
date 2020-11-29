85446f86c23102c827c1cfd17fa027a30fc37e46
Vincent de Almeida <vincent@decentralisedengineeringltd.co.uk>
2020-11-29 22:36:17 +0000

    ServiceHistory

---
+++ smart-contracts/contracts/ServiceHistory.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.7.5;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract ServiceHistory is ERC721("ServiceHistory", "SRV") {
+
+}
